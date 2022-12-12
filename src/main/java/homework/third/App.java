1e004e1c6faf2d8001141418fc23b51da2e59c88
Vladyslav Brovkov <brovkov777@gmail.com>
2022-12-12 13:35:17 +0200

    Final Commit with tests

--- src/main/java/homework/third/App.java
+++ src/main/java/homework/third/App.java
@@ -13,4 +13,9 @@ public class App {
         ViolationStatisticUtil.writeFineStatisticToFile(new File("src/main/resources/Task1Files/ViolationFiles/"),
                 new File("src/main/resources/Task1Files/violationStatParallel.xml"), numberOfThreads);
     }
+//    Program Results Data:
+//    Without parallel Threads | Program Time: 9.616
+//    Threads Number: 2 | Program Time: 6.994
+//    Threads Number: 4 | Program Time: 5.297
+//    Threads Number: 8 | Program Time: 4.843
 }
